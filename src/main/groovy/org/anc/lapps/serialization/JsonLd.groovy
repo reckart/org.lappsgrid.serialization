9906f5370057d707234c2788c36ceceb9b57b14b
chunqishi <shicq@cs.brandeis.edu>
2014-02-08 22:31:16 -0500

    improve the json

--- src/main/groovy/org/anc/lapps/serialization/JsonLd.groovy
+++ src/main/groovy/org/anc/lapps/serialization/JsonLd.groovy
@@ -62,21 +62,16 @@ class JsonLd {
     public JsonLd(content = null) {
         this.content = content
 
-//        def jsonapi = new JsonLdApi(Object input, Object context, JsonLdOptions opts)
-
-//        JsonLdOptions options = new JsonLdOptions();
-//        options.setBase()
-//        Object compact = JsonLdProcessor.compact(jsonObject, context, options);
-
-
         def clsname = getClassLastName(content)
         def root =  "class_" + clsname
         if (is_null(clsname)) {
 //            jsonld =
         } else if (is_str(clsname)) {
             jsonld = str2jsonld(root, content)
-        } else if (is_num(clsname)) {
-            jsonld = num2jsonld(root, content)
+        } else if (is_int(clsname)) {
+            jsonld = int2jsonld(root, content)
+        } else if (is_float(clsname)) {
+            jsonld = float2jsonld(root, content)
         } else if (is_bool(clsname)) {
             jsonld = bool2jsonld(root, content)
         } else if (is_map(clsname)) {
@@ -89,7 +84,7 @@ class JsonLd {
     }
 
     public String toString(){
-        return jsonld
+        return new JsonBuilder(jsonld).toString()
     }
 
     def getClassLastName(obj){
@@ -101,27 +96,55 @@ class JsonLd {
     def bool2jsonld(root, str) {
         def json = new JsonBuilder(str).toString()
         def jsonobj = JSONUtils.fromString(json)
-        jsonobj.put("@type", "xsd:boolean")
+//        jsonobj.put("@type", "xsd:boolean")
+//        configKeywords.eachWithIndex { String keywordEntry, int j ->
+//            def configPath = root + "." + keywordEntry
+//            def jsonconfig = getJsonObj(configPath)
+//            if (jsonconfig != null)
+//            // update jsonobj
+//                jsonobj.putAll(jsonconfig)
+//        }
         return jsonobj
     }
 
     def int2jsonld(root, str) {
         def json = new JsonBuilder(str).toString()
         def jsonobj = JSONUtils.fromString(json)
-        jsonobj.put("@type", "xsd:integer")
+//        jsonobj.put("@type", "xsd:integer")
+//        configKeywords.eachWithIndex { String keywordEntry, int j ->
+//            def configPath = root + "." + keywordEntry
+//            def jsonconfig = getJsonObj(configPath)
+//            if (jsonconfig != null)
+//            // update jsonobj
+//                jsonobj.putAll(jsonconfig)
+//        }
         return jsonobj
     }
 
     def float2jsonld(root, str) {
         def json = new JsonBuilder(str).toString()
         def jsonobj = JSONUtils.fromString(json)
-        jsonobj.put("@type", "xsd:double")
+//        jsonobj.put("@type", "xsd:double")
+//        configKeywords.eachWithIndex { String keywordEntry, int j ->
+//            def configPath = root + "." + keywordEntry
+//            def jsonconfig = getJsonObj(configPath)
+//            if (jsonconfig != null)
+//            // update jsonobj
+//                jsonobj.putAll(jsonconfig)
+//        }
         return jsonobj
     }
 
     def str2jsonld(root, str) {
         def json = new JsonBuilder(str).toString()
         def jsonobj = JSONUtils.fromString(json)
+//        configKeywords.eachWithIndex { String keywordEntry, int j ->
+//            def configPath = root + "." + keywordEntry
+//            def jsonconfig = getJsonObj(configPath)
+//            if (jsonconfig != null)
+//            // update jsonobj
+//                jsonobj.putAll(jsonconfig)
+//        }
         return jsonobj
     }
 
@@ -135,22 +158,24 @@ class JsonLd {
         def json = new JsonBuilder(map).toString()
         def jsonobj = JSONUtils.fromString(json)
         map.eachWithIndex { Map.Entry<Object, Object> entry, int i ->
-            if (is_null(entry.value)) {
+            def propertyroot = root + "." + entry.key
+            def valclsname = getClassLastName(entry.value)
+            if (is_null(valclsname)) {
                 // TODO: what to do with null in map?
-            } else if (is_bool(entry.value)) {
-                jsonobj.put(entry.key, bool2jsonld(entry.value))
-            } else if (is_str(entry.value)) {
-                jsonobj.put(entry.key, str2jsonld(entry.value))
-            } else if (is_int(entry.value)) {
-                jsonobj.put(entry.key, int2jsonld(entry.value))
-            } else if (is_float(entry.value)) {
-                jsonobj.put(entry.key, float2jsonld(entry.value))
-            } else if (is_map(entry.value)) {
-                jsonobj.put(entry.key, map2jsonld(entry.value))
-            } else if (is_list(entry.value)) {
-                jsonobj.put(entry.key, list2jsonld(entry.value))
+            } else if (is_bool(valclsname)) {
+                jsonobj.put(entry.key, bool2jsonld(propertyroot, entry.value))
+            } else if (is_str(valclsname)) {
+                jsonobj.put(entry.key, str2jsonld(propertyroot, entry.value))
+            } else if (is_int(valclsname)) {
+                jsonobj.put(entry.key, int2jsonld(propertyroot, entry.value))
+            } else if (is_float(valclsname)) {
+                jsonobj.put(entry.key, float2jsonld(propertyroot, entry.value))
+            } else if (is_map(valclsname)) {
+                jsonobj.put(entry.key, map2jsonld(propertyroot, entry.value))
+            } else if (is_list(valclsname)) {
+                jsonobj.put(entry.key, list2jsonld(propertyroot, entry.value))
             } else {
-                jsonobj.put(entry.key, obj2jsonld(entry.value))
+                jsonobj.put(entry.key, obj2jsonld(propertyroot, entry.value))
             }
         }
         return jsonobj
@@ -175,8 +200,8 @@ class JsonLd {
 
     def obj2jsonld(root, obj){
         String json = new JsonBuilder(obj).toString()
-        println "-------json------"
-        println json
+//        println "-------json------"
+//        println json
         def jsonobj = JSONUtils.fromString(json)
 
         configKeywords.eachWithIndex { String keywordEntry, int j ->
@@ -186,52 +211,30 @@ class JsonLd {
                 // update jsonobj
                 jsonobj.putAll(jsonconfig)
         }
-
-        def jsonid = getJsonObj(root + ".jsonld_id")
-        println "------id----------"
-        println jsonid
-        if (jsonid != null)
-            // update jsonobj
-            jsonobj.putAll(jsonid)
-
-        def jsoncontext = getJsonObj(root + ".jsonld_context")
-//        def base = get(root + ".jsonld_base")
-
-        println "------context----------"
-        println jsoncontext
-
-        JsonLdOptions opts = new JsonLdOptions()
-        println "-------jsonobj------"
-        println jsonobj
-
-        Object compact = JsonLdProcessor.compact(jsonobj, jsoncontext, opts);
-        println "--------------compact-------------"
-        println compact
-        return JSONUtils.toPrettyString(compact)
-//
-//
-//        configKeywords.eachWithIndex { String keywordEntry, int j ->
-//            def configPath = root + "." + keywordEntry
-//            println configPath
-//            println get(configPath)
-//        }
-//
-//        obj.getProperties().eachWithIndex { Map.Entry<Object, Object> entry, int i ->
-//            if (! ignoredProperties.contains(entry.key)) {
-//                root =  "class_" + clsname + "." + entry.key
-//                configKeywords.eachWithIndex { String keywordEntry, int j ->
-//                    def configPath = root + "." + keywordEntry
-////                    println configPath
-////                    println get(configPath)
-//                }
-//                println entry.key
-//                println getClassLastName(entry.value)
-//                println entry.value.getProperties()
-//            }
-//
-//        }
-
-
+        obj.getProperties().eachWithIndex { Map.Entry<Object, Object> entry, int i ->
+            if (!ignoredProperties.contains(entry.key)) {
+                def propertyroot = root + "." + entry.key
+                def valclsname = getClassLastName(entry.value)
+                if (is_null(valclsname)) {
+                    // TODO: what to do with null in map?
+                } else if (is_bool(valclsname)) {
+                    jsonobj.put(entry.key, bool2jsonld(propertyroot, entry.value))
+                } else if (is_str(valclsname)) {
+                    jsonobj.put(entry.key, str2jsonld(propertyroot, entry.value))
+                } else if (is_int(valclsname)) {
+                    jsonobj.put(entry.key, int2jsonld(propertyroot, entry.value))
+                } else if (is_float(valclsname)) {
+                    jsonobj.put(entry.key, float2jsonld(propertyroot, entry.value))
+                } else if (is_map(valclsname)) {
+                    jsonobj.put(entry.key, map2jsonld(propertyroot, entry.value))
+                } else if (is_list(valclsname)) {
+                    jsonobj.put(entry.key, list2jsonld(propertyroot, entry.value))
+                } else {
+                    jsonobj.put(entry.key, obj2jsonld(propertyroot, entry.value))
+                }
+            }
+        }
+        return jsonobj
     }
 
     def is_str(clsname){
--- src/test/groovy/org/anc/lapps/serialization/JsonLdTest.groovy
+++ src/test/groovy/org/anc/lapps/serialization/JsonLdTest.groovy
@@ -30,25 +30,7 @@ class JsonLdTest {
     }
     @Test
     public void testInit(){
-//        jld
         println '<-------- JsonLdTest ---------'
-
-//        def sjsonld = """
-//            {
-//              "@context": {
-//                "ical": "http://www.w3.org/2002/12/cal/ical#",
-//                "xsd": "http://www.w3.org/2001/XMLSchema#",
-//                "ical:dtstart": {
-//                  "@type": "xsd:dateTime"
-//                }
-//              },
-//              "ical:summary": "Lady Gaga Concert",
-//              "ical:location": "New Orleans Arena, New Orleans, Louisiana, USA",
-//              "ical:dtstart": "2011-04-09T20:00Z"
-//            }
-//        """
-//        def jsonobj = JSONUtils.fromString(sjsonld)
-
         JsonLd jld = new JsonLd()
         Annotation antn = new Annotation()
         def jld2 = new JsonLd(antn)
