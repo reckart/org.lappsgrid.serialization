23777994e895f84f553b40e1a7ee87db7adba493
Keith Suderman <suderman@anc.org>
2015-02-06 22:20:46 -0500

    Still removing references to the Constants class.

--- src/test/groovy/org/lappsgrid/serialization/ExampleGenerator.groovy
+++ src/test/groovy/org/lappsgrid/serialization/ExampleGenerator.groovy
@@ -1,9 +1,9 @@
 package org.lappsgrid.serialization
 
-import org.lappsgrid.discriminator.Constants
 import org.lappsgrid.serialization.lif.Annotation
 import org.lappsgrid.serialization.lif.Container
 import org.lappsgrid.serialization.lif.View
+import static org.lappsgrid.discriminator.Discriminators.Uri
 
 /**
  * @author Keith Suderman
@@ -26,13 +26,13 @@ class ExampleGenerator {
         //                012345678901
         container.text = 'Hello world.'
         View tokens = new View()
-        tokens.addContains('Token', this.class.name, Constants.Uri.TOKEN)
+        tokens.addContains('Token', this.class.name, Uri.TOKEN)
         tokens.annotations << token(0, 5, [pos:'UH',lemma:'hello',string:'hello'])
         tokens.annotations << token(6, 11, [pos:'NN',lemma:'world',string:'world'])
         tokens.annotations << token(11, 12, [pos:'PUNCT',string:'.'])
 
         View sentences = new View()
-        sentences.addContains('Sentence', this.class.name, Constants.Uri.SENTENCE)
+        sentences.addContains('Sentence', this.class.name, Uri.SENTENCE)
         Annotation sentence = new Annotation(id:'s1', start:0, end:12, label:'Sentence')
         sentences.add(sentence)
 
@@ -49,15 +49,15 @@ class ExampleGenerator {
         container.text = 'Hello world.'
         View tokens = new View()
         tokens.metadata.annotations = [:]
-        tokens.metadata.annotations[Annotations.TOKEN] = [producer:this.class.name]
+        tokens.metadata.annotations[Uri.TOKEN] = [producer:this.class.name]
         tokens.annotations << token(0, 5, [pos:'UH',lemma:'hello',string:'hello'])
         tokens.annotations << token(6, 11, [pos:'NN',lemma:'world',string:'world'])
         tokens.annotations << token(11, 12, [pos:'PUNCT',string:'.'])
 
         View sentences = new View()
         sentences.metadata.annotations = [:]
-        sentences.metadata.annotations[Annotations.SENTENCE] = [producer: this.class.name]
-        Annotation sentence = new Annotation(id:'s1', start:0, end:12, label:Annotations.SENTENCE)
+        sentences.metadata.annotations[Uri.SENTENCE] = [producer: this.class.name]
+        Annotation sentence = new Annotation(id:'s1', start:0, end:12, label:Uri.SENTENCE)
         sentences.add(sentence)
 
         container.addView(tokens)
@@ -73,7 +73,7 @@ class ExampleGenerator {
         a.id = "t${tokenCounter}"
         a.start = start
         a.end = end
-        a.label = Annotations.TOKEN
+        a.label = Uri.TOKEN
         a.features = features
         return a
     }
